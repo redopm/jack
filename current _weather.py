f27519bb46dcc03152278d6175325cfa7119596c
redopm <omprakashmaury001@gmail.com>
2020-04-02 03:18:30 +0530

    apr 2

--- current _weather.py
+++ current _weather.py
@@ -5,17 +5,7 @@ from pyowm import OWM
 import re
 import pyttsx3
 from pyowm.caches.lrucache import LRUCache
-from datetime import datetime 
-import pytz 
-  
-  
-tz_INDIA = pytz.timezone('Asia/Kolkata')  
-datetime_INDIA = datetime.now(tz_INDIA) 
-date = datetime_INDIA.strftime("%H:%M:%S")
 
-MONTHS = ['january', '']
-DAYS = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
-DAY_EXTENTIONS = ["rd", "th", "st", "nd"]
 # text to speak by jack 
 def speak(text):
     engine = pyttsx3.init()
@@ -38,56 +28,13 @@ def get_audio():
         except Exception as e:
             print(Exception, e)
     return said.lower()
-
-def get_date(text):
-    text = text.lower()
-    today = datetime.date.today()
-
-    if text.count("today") > 0:
-        return today
-
-    day = -1
-    day_of_week = -1
-    mount = -1
-    year = today.year
-
-    for word in text.split():
-        if word in MONTHS:
-            month - MONTHS.index(word) + 1
-        elif word in DAYS:
-            day_of_week = DAYS.index(word)
-        elif word.isdigit():
-            day = int(word)
-        else:
-            for ext in DAY_EXTENTIONS:
-                found - word.find(ext)
-                if found>0:
-                    try:
-                        day - int(word[:found])
-                    except:
-                        pass
-    if month < today.month and month != -1:
-        year = year+1
-
-    if month == -1 and day != -1:
-        if day < today.day:
-            month = today.month + 1
-        else:
-            month = today.month
-    if month == -1 and day == -1 and day_of_week != -1:
-        current_day_of_week - today.weekday()
-        dif = day_of_week - current_day_of_week
-
-        if dif < 0:
-            dif +=7
-
-
+    
 def weather():
     API_key='ab0d5e80e8dafb2cb81fa9e82431c1fa'
     owm = OWM(API_key)
     #cache = LRUCache()
     obs = owm.weather_at_place('Jaipur')
-    w = obs.get_weather(date)
+    w = obs.get_weather()
     k = w.get_detailed_status()
     x = w.get_reference_time(timeformat='iso') 
     print(w)
---
+++ event_founder.py
@@ -0,0 +1,56 @@
+
+
+
+MONTHS = ['january', 'February', 'March', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+DAYS = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+DAY_EXTENTIONS = ["rd", "th", "st", "nd"]
+
+
+def get_date(text):
+    text = text.lower()
+    today = datetime.date.today()
+
+    if text.count("today") > 0:
+        return today
+
+    day = -1
+    day_of_week = -1
+    month = -1
+    year = today.year
+
+    for word in text.split():
+        if word in MONTHS:
+            month - MONTHS.index(word) + 1
+        elif word in DAYS:
+            day_of_week = DAYS.index(word)
+        elif word.isdigit():
+            day = int(word)
+        else:
+            for ext in DAY_EXTENTIONS:
+                found - word.find(ext)
+                if found > 0:
+                    try:
+                        day - int(word[:found])
+                    except:
+                        pass
+    if month < today.month and month != -1:
+        year = year+1
+
+    if month == -1 and day != -1:
+        if day < today.day:
+            month = today.month + 1
+        else:
+            month = today.month
+    if month == -1 and day == program-1 and day_of_week != -1:
+        current_day_of_week - today.weekday()
+        dif = day_of_week - current_day_of_week
+
+        if dif < 0:
+            dif +=7
+            if text.count("next") >= 1:
+                dif += 7
+        return today + datetime.timedelta(dif)
+    if day != -1:
+        return datetime.date(month = month, day = day, year = year)
+
+
